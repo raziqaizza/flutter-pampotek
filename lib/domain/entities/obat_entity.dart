50225f5148ae6a11882255d92368996237a60436
stfRangga <stefanusrangga04@gmail.com>
2024-12-04 00:26:22 +0700

    Fixing Obat Entity

---
+++ lib/domain/entities/obat_entity.dart
@@ -0,0 +1,14 @@
+class ObatEntity {
+  final String id;
+  final String namaObat;
+  final String deskripsiObat;
+  final int jumlahObat;
+  final int hargaObat;
+
+  ObatEntity(
+      {required this.id,
+      required this.namaObat,
+      required this.deskripsiObat,
+      required this.jumlahObat,
+      required this.hargaObat});
+}
